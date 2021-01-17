bb71612ad644160c98a98b390bf914251f9a6eba
Warwick Stone <uozu.aho@gmail.com>
2021-01-17 21:58:16 +1100

    add box office table

--- db_setup/schema.sql
+++ db_setup/schema.sql
@@ -5,3 +5,14 @@ CREATE TABLE movies (
     year int NOT NULL,
     length_minutes int NOT NULL
 );
+
+CREATE TABLE box_office (
+    movie_id int not null,
+    rating float not null,
+    domestic_sales bigint not null,
+    international_sales bigint not null,
+    constraint fk_movie
+        foreign key(movie_id)
+        references movies(id)
+        on delete cascade
+);
